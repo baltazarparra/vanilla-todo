bec5eee5ff424bd5776218f3ff319759ada4a384
baltazarparra <baltazarparra@outlook.com>
2016-09-30 06:10:09 -0300

    first release

--- main.js
+++ main.js
@@ -13,26 +13,38 @@
             },
 
             initEvents: function initEvents() {
-                $addButton.addEventListener('click', app.createItem);                
+                $addButton.addEventListener('click', app.createItem);
+                $inputItem.addEventListener('keypress', function(e) {
+                    if (e.keyCode == 13)
+                        app.createItem();
+                });
             },
 
             createItem: function createItem() {
+
                 var $item = document.createElement('li');
+                $item.setAttribute("data-js", "listItem");
+
                 var $check = document.createElement('input');
-                var $label = document.createElement('label');
-                var $inputValue = $inputItem.value;
-                var $inputText = document.createTextNode($inputValue);
-                var $removeButton = document.createElement('button');
-                var $textRemoveButton = document.createTextNode('x');
                 $check.setAttribute('type', 'checkbox');
+
+                var $label = document.createElement('label');
+
                 $item.appendChild($check);
                 $item.appendChild($label);
-                $label.appendChild($inputText);
-                $item.setAttribute("data-js", "listItem");
-                $removeButton.appendChild($textRemoveButton);
+
+                var $removeButton = document.createElement('button');
+                var $textRemoveButton = document.createTextNode('x');
                 $removeButton.setAttribute("data-js", "removeButton");
+
+                var $inputValue = $inputItem.value;
+                var $inputText = document.createTextNode($inputValue);
+                $label.appendChild($inputText);
+
+                $removeButton.appendChild($textRemoveButton);
                 $todoList.appendChild($item);
                 $item.appendChild($removeButton);
+
                 $removeButton.addEventListener('click', app.removeItem);
                 $inputItem.value = '';
             },
